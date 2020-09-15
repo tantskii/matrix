ebba0adcc5c62d18cf5df6a7fb090e83d20be151
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-15 22:38:21 +0000

    Deploy tantskii/matrix to github.com/tantskii/matrix.git:gh-pages

--- search/all_2.js
+++ search/all_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['cmakelists_2etxt',['CMakeLists.txt',['../_c_make_lists_8txt.html',1,'']]],
-  ['contains',['contains',['../class_data.html#a15012d998389851b9897d7f718aaad6e',1,'Data']]]
+  ['contains',['contains',['../class_data.html#a17aac4288594d13c9727407f5e9ba159',1,'Data']]]
 ];
--- search/all_4.js
+++ search/all_4.js
@@ -4,5 +4,5 @@ var searchData=
   ['element',['Element',['../class_data.html#a7baf4ddb6dcfa94bf2e7bab93fbae010',1,'Data']]],
   ['elementtype',['ElementType',['../data__helpers_8h.html#a2160803a23997b356f5cdf9f74a3f915',1,'data_helpers.h']]],
   ['end',['end',['../class_data.html#a86fb0ff46cb84f83d70c40a63bde8042',1,'Data::end()'],['../class_matrix.html#aa53db635af14f6dbb29763bb9a832d80',1,'Matrix::end()']]],
-  ['erase',['erase',['../class_data.html#adb60b6b534f6978efa86f33f8ca3dec3',1,'Data']]]
+  ['erase',['erase',['../class_data.html#adb60b6b534f6978efa86f33f8ca3dec3',1,'Data::erase(const Key &amp;key)'],['../class_data.html#a44cddfd096d2503e19c7629925456a07',1,'Data::erase(MapIt it)']]]
 ];
